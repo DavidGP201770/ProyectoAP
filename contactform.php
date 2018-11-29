9393104f787543448352254c4e6a134d7a32b37d
David Uriel García Pérez <david_uriel_garcia@hotmail.com>
2018-11-29 12:34:05 -0600

    UPDATE correo

--- contactform.php
+++ contactform.php
@@ -1,11 +1,14 @@
 <?php
-	if(isset($_POST['submit'])){
+	
+	if(isset($_POST['enviar']))
+	{
 		$name=$_POST['name'];
 		$tele=$_POST['tele'];
 		$subject=$_POST['subject'];
 		$mailform=$_POST['mail'];
-		$messagg=$_POST['message'];
+		$message=$_POST['message'];
 	}
+
 	$mailTo="davicho201170@gmail.com";
 	$headers="De: ".$mailform;
 	$txt="Telefono: ".$tele.".\n\n Haz recibido un correo electronico de ".$name.".\n\n".$message;
